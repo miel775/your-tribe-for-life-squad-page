087ef8c31e7833228fbb667e1760ca82d16b54d0
Naddybsx <Nadd_bs@icloud.com>
2025-09-05 03:26:38 +0200

    feat(routes): render member details #22

---
+++ my-app/src/routes/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<!-- Hier importeer ik de (member)data die ik in +page.server.js heb opgehaald -->
+<script>
+    let { data } = $props();
+    const member = data.member;
+</script>
+
+<!-- Hier wordt de data van de opgehaalde member getoond --> 
+    <h2>{member.name}</h2>
+    <a href="/">Terug naar overzicht</a>
