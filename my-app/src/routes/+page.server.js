63b327005d01975d80403f1c4ff79be31ac4b647
Naddybsx <Nadd_bs@icloud.com>
2025-09-05 02:58:04 +0200

    feat(routes): fetch members data #22

---
+++ my-app/src/routes/+page.server.js
@@ -0,0 +1,6 @@
+export async function load({ url }) {
+    const membersResponse = await fetch("https://fdnd.directus.app/items/person?fields=*,squads.squad_id.name,squads.squad_id.cohort,squads.squad_id.tribe.name&filter[squads][squad_id][cohort][_eq]=2526&filter[squads][squad_id][tribe][name][_eq]=FDND%20Jaar%202");
+    const membersData = await membersResponse.json();
+
+    return {members: membersData.data};
+}
