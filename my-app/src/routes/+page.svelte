72f3c429a36ce0546bc01d4368dd1e1f85187281
Naddybsx <Nadd_bs@icloud.com>
2025-09-05 03:34:30 +0200

    feat(view): add dynamic member links #22

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -6,5 +6,7 @@
 
 <!-- Hier itereer ik met een loop door alle members heen -->
 {#each members as member}
-    <h2>{member.name}</h2>
+    <h2>
+     <a href={`/${member.id}`}>{member.name}</a>
+    </h2>
 {/each}
