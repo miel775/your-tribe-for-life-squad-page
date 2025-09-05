18ad166be5f33ae8c7a8974ff3f06056d1bbf726
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-09-05 10:52:36 +0200

    netify

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
