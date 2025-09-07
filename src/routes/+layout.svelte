dbf8f8ea4ec3bb814c0680c69326aae8f75d3c54
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-09-07 15:23:20 +0200

    Map verplaatst
    Voor de main heb ik nu de map my-app verplaatst zodat er geen dubbele dingen in staan. Het werkt met 'npm run dev'
    
    #5

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<style>
+	body {
+		background-color: darkblue;
+	}
+</style>
+
+{@render children?.()}
