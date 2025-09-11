5a424c5472c3fcad07b41fa845e44d3785e6bffc
Naddybsx <Nadd_bs@icloud.com>
2025-09-11 13:51:06 +0200

    (feat) responsive book layout #61

--- src/routes/members/[id]/+page.svelte
+++ src/routes/members/[id]/+page.svelte
@@ -20,3 +20,39 @@
         </article>
     </div>
 </main>
+
+<style>
+    main {
+    display: grid;
+    place-items: center;
+    background-color: rgb(255, 255, 255);
+    padding: 2rem;
+    min-height: 100vh;
+    }
+
+    img {
+        width: 100%;
+        aspect-ratio: 3/4;
+        object-fit: cover;
+        border-radius: .5em;
+    }
+
+    .book {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 1rem;
+    }
+    .page {
+        border: 6px solid rgb(52, 20, 6);
+        border-radius: 1em;
+        background-color: white;
+        padding: 1em;
+    }
+
+    @media (min-width: 720px) {
+        .book {
+            grid-template-columns: 1fr 1fr;
+             width: min(1000px, 95vw);
+        }
+    }
+</style>
