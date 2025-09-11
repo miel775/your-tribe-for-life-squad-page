3a99e3090bf67507eaf0e5ff4fb91878c70993ec
Akikosophia <asschermer@hotmail.com>
2025-09-11 22:44:28 +0200

    #45 feat html structuur

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,15 +1,32 @@
 <!--  Hier importeer ik de data die ik in +page.server.js heb opgehaald -->
 <script>
-    let { data } = $props();
-    const members = data.members;
+  let { data } = $props();
+  const members = data.members;
 </script>
 
 <h1>Overzichtspagina</h1>
+<div class="student-flag"></div>
 
 <!-- Hier itereer ik met een loop door alle members heen -->
-{#each members as member}
-    <h2>
-     <a href={`/members/${member.id}`}>{member.name}</a>
-    </h2>
-{/each}
+<section class="book">
+  <h2 class="book-title">Squadpage</h2>
+  {#each members as member}
+    <ul class="students">
+      <li class="student">
+        <img
+          src={member.avatar}
+          class="student-img"
+          alt="Avatar van {member.name}"
+        />
+        <a href={`/members/${member.id}`}>
+            <h3 class="student-title">{member.name}</h3>  </a>
+      </li>
+    </ul>
+  {/each}
+</section>
 
+<style>
+
+
+
+</style>
